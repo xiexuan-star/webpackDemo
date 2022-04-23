928a3a03809964f265709962a65246f63293c612
xiexuan-star <30561277+xiexuan-star@users.noreply.github.com>
2022-04-23 18:45:37 +0800

    Update index.js
    feat: add tryExtensions function

--- core/utils/index.js
+++ core/utils/index.js
@@ -4,8 +4,26 @@
  * @param {*} path
  * @returns
  */
+const fs = require('fs');
+
 function toUnixPath(path) {
   return path.replace(/\\/g, '/');
 }
 
-module.exports = { toUnixPath };
+/**
+ * @description 尝试通过配置中的extensions去匹配没有后缀名的文件
+ * @param {string} modulePath
+ * @param {string[]} extensions
+ * @param {string} originModulePath
+ * @param {string} moduleContext
+ */
+function tryExtensions(modulePath, extensions, originModulePath, moduleContext) {
+  if (fs.existsSync(modulePath)) return modulePath;
+  for (let i = 0; i < extensions.length; i++) {
+    const pathWithExtension = modulePath + extensions[i];
+    if (fs.existsSync(pathWithExtension)) return pathWithExtension;
+  }
+  throw new Error(`No module: Can\'t resolve ${originModulePath} in ${moduleContext}`);
+}
+
+module.exports = { toUnixPath, tryExtensions };
